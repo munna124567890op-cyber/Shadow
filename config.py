8e6a98331669a3f4535c257841b4d602258cb2cc
munna124567890op-cyber <munna124567890op@gmail.com>
2025-10-04 19:31:52 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = "26269144"
+API_HASH = "8d70e5e932f9f30d5af8ad39c6b8ece5"
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+MONGO_URL = getenv("MONGO_URL", None)
+OWNER_ID = "8421110576"
+SUPPORT_GRP = "+KO3LMVgneLIwNzFl"
+UPDATE_CHNL = "LOVE_BFF_PFP"
+OWNER_USERNAME = "i_t_z_broken"
